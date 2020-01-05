f9593458cd13b2968abd6048bad73064d8cb29d0
Logan Moss <lrdjmoss@gmail.com>
2020-01-05 15:36:02 -0800

    made working web page and linked jquery javascript files together

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var timeLeft = 60;
+
+
+$(document).ready(function() {
+    
+    setInterval(function(){
+        timeLeft -= 1;
+        // console.log(timeLeft);
+        $("#timer").text("Time Remaining: " + timeLeft);
+        if(timeLeft == 0){
+            // alert("Submit form here");
+
+        }
+    },1000);    
+
+    $("#submitbutton").on("click", function(event){
+        event.preventDefault();
+
+        let questionOne = $("input[name='question1']:checked").val();
+        let questionTwo = $("input[name='question2']:checked").val();
+        let questionThr = $("input[name='question3']:checked").val();
+        let questionFou = $("input[name='question4']:checked").val();
+        let questionFiv = $("input[name='question5']:checked").val();
+        let questionSix = $("input[name='question6']:checked").val();
+
+        let answerArr = [questionOne, questionTwo, questionThr, questionFou, questionFiv, questionSix];
+        for(let i = 0; i < answerArr.length; i++){
+            if(answerArr[i] == "correct"){
+                correctAnswers++;
+            }
+        }
+        alert(correctAnswers);
+        var body = $("body");
+        body.empty();
+        newDiv = $("<div>New div!</div>");
+        body.append(newDiv);
+    });
+});
